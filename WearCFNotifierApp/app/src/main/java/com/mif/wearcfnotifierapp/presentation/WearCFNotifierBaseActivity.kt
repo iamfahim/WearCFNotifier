6eb26d93d7ab1185bd67b0b6f7396e104d1e0179
iamfahim <mislamfahim@gmail.com>
2024-09-28 13:51:27 +0600

    Project initialization

---
+++ WearCFNotifierApp/app/src/main/java/com/mif/wearcfnotifierapp/presentation/WearCFNotifierBaseActivity.kt
@@ -0,0 +1,64 @@
+package com.mif.wearcfnotifierapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import com.mif.wearcfnotifierapp.presentation.theme.WearCFNotifierAppTheme
+
+class WearCFNotifierBaseActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        setTheme(android.R.style.Theme_DeviceDefault)
+
+        setContent {
+            WearCFNotifierApp()
+        }
+    }
+}
+
+@Composable
+fun WearCFNotifierApp() {
+    WearCFNotifierAppTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            TimeText()
+            LazyColumnCFContestItem()
+        }
+    }
+}
+
+@Composable
+fun LazyColumnCFContestItem() {
+    LazyColumn {
+        items((1..20).toList()) {
+            Text(text = "CF Contest $it")
+        }
+    }
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearCFNotifierApp()
+}
